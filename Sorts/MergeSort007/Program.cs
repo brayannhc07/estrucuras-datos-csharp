365e9f297d213a4f8c188c9aa25bb1a782e2bd10
Bryan <brayannhc07@gmail.com>
2021-10-03 16:08:17 -0500

    Metodos de Ordenamiento

---
+++ Sorts/MergeSort007/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace MergeSort007 {
+    class Program {
+        private static CListaLigada miLista = new CListaLigada();
+        static void Main(string[] args) {
+
+            // CListaLigada l = new CListaLigada();
+            // l.Adicionar(6);
+            // l.Adicionar(7);
+            // l.Adicionar(9);
+            // l.Adicionar(11);
+
+            // CListaLigada r = new CListaLigada();
+            // r.Adicionar(8);
+            // r.Adicionar(10);
+            // r.Adicionar(12);
+            // r.Adicionar(13);
+
+            // CListaLigada merged = Merge(l, r);
+
+            // merged.Transversa();
+
+            ////////////////////////////////////////
+
+            miLista.Adicionar(3);
+            miLista.Adicionar(15);
+            miLista.Adicionar(7);
+            miLista.Adicionar(19);
+            miLista.Adicionar(11);
+            miLista.Adicionar(1);
+
+            miLista.Transversa();
+
+            CListaLigada ordenada = MergeSort(miLista);
+            ordenada.Transversa();
+        }
+
+        public static CListaLigada Merge(CListaLigada listaIzq, CListaLigada listaDer) {
+            //  Para que funcione correctamente las listas debe estar ordenadas
+
+            // lista donde se unen
+            CListaLigada unida = new CListaLigada();
+
+            // Indices en cada lista
+            int indiceI = 0;
+            int indiceD = 0;
+
+            // Cantidad de elementos en cada lista
+            int cantI = listaIzq.Cantidad();
+            int cantD = listaDer.Cantidad();
+
+            // Recorremos mientras las dos listas tengan elementos sin procesar
+            while (indiceI < cantI && indiceD < cantD) {
+                // Si el de la izquierda es menor o igual, adicionamos el de la izquierda
+                if (listaIzq[indiceI] <= listaDer[indiceD]) {
+                    unida.Adicionar(listaIzq[indiceI]);
+
+                    // Avanzamos el índice izquierdo
+                    indiceI++;
+                } else {
+                    // Si el de la derecha es menor, adicionamos el de la derecha
+                    unida.Adicionar(listaDer[indiceD]);
+
+                    // Avanzamos el índice derecho
+                    indiceD++;
+                }
+            }
+
+            // Si sobraron elementos en la lista izquierda, los ponemos todos
+            while (indiceI < cantI) {
+                unida.Adicionar(listaIzq[indiceI]);
+                indiceI++;
+            }
+            // Si sobraron elementos en la lista derecha, los ponemos todos
+            while (indiceD < cantD) {
+                unida.Adicionar(listaDer[indiceD]);
+                indiceD++;
+            }
+
+            // Regresamos la lista unida
+            return unida;
+        }
+
+        public static CListaLigada MergeSort(CListaLigada pLista) {
+            // Cantidad de elementoss en la lista
+            int cantidad = pLista.Cantidad();
+            int n = 0;
+
+            // Caso base, una lista de un solo elemento ya está ordenado
+            if (cantidad < 2) {
+                return pLista;
+            }
+
+            // Obtenemos el punto medio de la lista
+            int mitad = cantidad / 2;
+
+            CListaLigada izquierda = new CListaLigada();
+            CListaLigada derecha = new CListaLigada();
+
+            // Adicionamos a la izquierda desde el inicio hasta antes de la mitad
+            for (n = 0; n < mitad; n++) {
+                izquierda.Adicionar(pLista[n]);
+            }
+
+            for (n = mitad; n < cantidad; n++) {
+                derecha.Adicionar(pLista[n]);
+            }
+
+            // Casos inductivos
+            // Hacemos el MergeSort de las listas Izquierda y Derecha
+            CListaLigada tempI = MergeSort(izquierda);
+            CListaLigada tempD = MergeSort(derecha);
+
+            // Hacemos el merge de lo que nos regresa el caso inductivo
+            CListaLigada ordenada = Merge(tempI, tempD);
+
+            // Regresamos la lista ordenada
+            return ordenada;
+        }
+    }
+}
---
+++ Sorts/QuickSort008/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace QuickSort008 {
+    class Program {
+        private static CListaLigada miLista = new CListaLigada();
+        static void Main(string[] args) {
+            miLista.Adicionar(3);
+            miLista.Adicionar(15);
+            miLista.Adicionar(7);
+            miLista.Adicionar(19);
+            miLista.Adicionar(11);
+            miLista.Adicionar(1);
+
+            miLista.Transversa();
+
+            QuickSort(0, miLista.Cantidad() - 1);
+
+            miLista.Transversa();
+        }
+
+        private static void Swap(int i1, int i2) {
+            int temp = miLista[i1];
+            miLista[i1] = miLista[i2];
+            miLista[i2] = temp;
+        }
+
+        public static int Particion(int pInicio, int pFin) {
+            int pivote = 0;
+            int iPivote = 0;
+            int n = 0;
+
+            // Seleccionamos el último pivote
+            pivote = miLista[pFin];
+
+            // Ponemos el índice de pivote con el índice del inicio
+            iPivote = pInicio;
+
+            // Recorremos la lista de pivote con el indice de inicio
+            for (n = pInicio; n < pFin; n++) {
+                // Si el elemento en el índice n es menor o igual al pivote 
+                if (miLista[n] <= pivote) {
+                    // Intercambiamos el elemento en n con el que se encuentre en el indice de pivote
+                    Swap(n, iPivote);
+
+                    // Incrementamos el indice del pivote
+                    iPivote++;
+                }
+            }
+
+            // Hacemos el swap final para colocar el pivote donde corresponde
+            Swap(iPivote, pFin);
+
+            // Regresamos el índice del pivote
+            return iPivote;
+        }
+
+        public static void QuickSort(int pInicio, int pFin) {
+            int iPivote = 0;
+
+            // Caso base, un elemento o fragmento inválido
+            if (pInicio >= pFin) return;
+
+            // Obtenemos el índice del pivote para el fragmento con el que trabajamos
+            iPivote = Particion(pInicio, pFin);
+
+            // Casos inductivos 
+            QuickSort(pInicio, iPivote - 1); // Fragmento a la izquierda del pivote
+            QuickSort(iPivote + 1, pFin); // Fragmento a la derecha del pivote
+        }
+    }
+}
